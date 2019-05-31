3aa9a4a8e5bcf1b56a7c7d3a30ed6f6141812cbf
Jess Smith <smith.jessk@gmail.com>
2019-05-31 16:56:55 -0400

    Refactor some more

--- src/main/scala/example/data.scala
+++ src/main/scala/example/data.scala
@@ -1,12 +1,17 @@
 package com.terainsights.napoca.data
 
-import java.util.UUID
 import java.time.Instant
 
+case class PriorityInfo(now: Instant)
+
+/**
+  * We assume that all IDs are coercible to a string type. We thought about adding
+  * type parameters, but this doesn't seem like a critical feature now and adds
+  * less benefit than, say, writing tests and coming up with a simpler API.
+  */
 case class BatchJob(id: String, brickID: String,
-                    requiredIntervals: Int, schedulingUserID: String,
-                    requiredHosts: Int, inserted: Instant,
-                    adminSetPriority: Long)
+                    requiredIntervals: Int, requiredHosts: Int,
+                    priority: PriorityInfo => Long)
 
 case class ScheduledBatchJob(hostToNumInstances: Map[String, Int],
                              startInterval: Int,
--- src/main/scala/example/logic.scala
+++ src/main/scala/example/logic.scala
@@ -14,190 +14,179 @@ case class HostWithCurrentUsage(host: Host, usage: HostResources)
   what are reasonable possible future versions of this constructor?
 
   why is this a class? doesn't that just increase bug risk?
-
-  concept to extract: Host * BatchJob * AlreadyScheduledthisround * interval
   */
-class Scheduler(hosts: Set[Host], brickIDToUsage: Map[String, HostResources],
-                runningJobs: List[RunningBatchJob], intervalsToLookAt: Range,
-                jobsToSchedule: List[BatchJob],
-                hostResCreator: HostResourcesCreator,
-                costToSchedule: HostWithCurrentUsage => Long) {
-
-  // insight: this can be used to reduce room for calling error; maybe? https://github.com/milessabin/shapeless/wiki/Feature-overview:-shapeless-2.0.0#coproducts-and-discriminated-unions
 
 
-  case class PotentialMatch(h: Host, toSchedule: BatchJob, interval: Int,
-                            alreadyScheduledThisRound: List[ScheduledBatchJob])
 
-  private def getTotalResourceUsage(p: PotentialMatch) = {
-    val relevantBrickIDNumInstances = runningJobs.foldLeft(List[(String, Int)]()){ (l, job) =>
-      val inBounds = job.s.startInterval + job.s.b.requiredIntervals > p.interval
-      job.s.hostToNumInstances.get(p.h.id) match {
-        case Some(count) if inBounds => (job.s.b.brickID, count) :: l
-        case _ => l
-      }
-    } ++ p.alreadyScheduledThisRound.foldLeft(List[(String, Int)]()){ (l, job) =>
-      val inBounds =
-        (job.startInterval + job.b.requiredIntervals > p.interval ||
-           p.interval + p.toSchedule.requiredIntervals > job.startInterval)
-      job.hostToNumInstances.get(p.h.id) match {
-        case Some(count) if inBounds => (job.b.brickID, count) :: l
-        case _ => l
-      }
+case class PotentialMatch(h: Host, toSchedule: BatchJob, interval: Int,
+                          alreadyScheduledThisRound: List[ScheduledBatchJob])
+
+private def getTotalResourceUsage(p: PotentialMatch) = {
+  val relevantBrickIDNumInstances = runningJobs.foldLeft(List[(String, Int)]()){ (l, job) => val inBounds = job.s.startInterval + job.s.b.requiredIntervals > p.interval
+    job.s.hostToNumInstances.get(p.h.id) match {
+      case Some(count) if inBounds => (job.s.b.brickID, count) :: l
+      case _ => l
     }
-    relevantBrickIDNumInstances.foldLeft(
-      hostResCreator.empty){
-      (accum, tuple) =>
-      val (brickID, cnt) = tuple
-      // safe b/c we generate this map
-      val hostUsage = brickIDToUsage(brickID)
-      Range.inclusive(1, cnt).foldLeft(accum)((x, y) => x + hostUsage)
+  } ++ p.alreadyScheduledThisRound.foldLeft(List[(String, Int)]()){ (l, job) =>
+    val inBounds =
+      (job.startInterval + job.b.requiredIntervals > p.interval ||
+         p.interval + p.toSchedule.requiredIntervals > job.startInterval)
+    job.hostToNumInstances.get(p.h.id) match {
+      case Some(count) if inBounds => (job.b.brickID, count) :: l
+      case _ => l
     }
   }
+  relevantBrickIDNumInstances.foldLeft(
+    hostResCreator.empty){
+    (accum, tuple) =>
+    val (brickID, cnt) = tuple
+    // safe b/c we generate this map
+    val hostUsage = brickIDToUsage(brickID)
+    Range.inclusive(1, cnt).foldLeft(accum)((x, y) => x + hostUsage)
+  }
+}
 
-  /**
-    analysis:
-    assumptions about the rest of the propram:
-    1) assumes that hosts will never be scheduled in groups s.t. they HAVE to be
-       scheduled in groups; i.e., if a job required its hosts to be scheduled in groups
-       of 2s, this function could still return 3; this is maybe misleading since we pass
-       the whole =toSchedule= object in, which may contain lots of metadata
-    2) that the brick IDs are strings (generics might help here; would that be hard in scala?)
-    schedule; e.g., we could have a mandatory cleanup round and want to denote this;
-    or we may want to add a round that can't be shut down; maybe it's only in this round for
-    some amount of time? wildcard match on the job state created this assumption
-
-
-    retval = number of jobs of this type that are schedulable on this host at this time
-    assumes that the host already contains the information about "its schedule"
-    "its schedule" = the jobs that are running on it both now and in the future
-    O(jobs + devices)
-
-    */
-  private def numHostsSchedulableHere(p: PotentialMatch): Int = {
-    val resourcesUsed: HostResources = getTotalResourceUsage(p)
-    val resourcesLeft: HostResources = p.h.capabilities - resourcesUsed
-    val jobResourcesOneInstance = brickIDToUsage(p.toSchedule.brickID)
-    val nCPUs = Math.floor(resourcesLeft.vcpus / jobResourcesOneInstance.vcpus)
-    val mem = Math.floor(resourcesLeft.memory / jobResourcesOneInstance.memory)
-    val dev = resourcesLeft.devices.foldLeft(Integer.MAX_VALUE){
-      (max, tuple) =>
-      val (name, cnt) = tuple
-      jobResourcesOneInstance.devices.get(name) match {
-        case None => max
-        case Some(required) =>
-          Math.min(max, Math.floor(cnt / required).toInt)
-      }
+/**
+  analysis:
+  assumptions about the rest of the propram:
+  1) assumes that hosts will never be scheduled in groups s.t. they HAVE to be
+  scheduled in groups; i.e., if a job required its hosts to be scheduled in groups
+  of 2s, this function could still return 3; this is maybe misleading since we pass
+  the whole =toSchedule= object in, which may contain lots of metadata
+  2) that the brick IDs are strings (generics might help here; would that be hard in scala?)
+  schedule; e.g., we could have a mandatory cleanup round and want to denote this;
+  or we may want to add a round that can't be shut down; maybe it's only in this round for
+  some amount of time? wildcard match on the job state created this assumption
+  
+  
+  retval = number of jobs of this type that are schedulable on this host at this time
+  assumes that the host already contains the information about "its schedule"
+  "its schedule" = the jobs that are running on it both now and in the future
+  O(jobs + devices)
+  
+  */
+private def numHostsSchedulableHere(p: PotentialMatch): Int = {
+  val resourcesUsed: HostResources = getTotalResourceUsage(p)
+  val resourcesLeft: HostResources = p.h.capabilities - resourcesUsed
+  val jobResourcesOneInstance = brickIDToUsage(p.toSchedule.brickID)
+  val nCPUs = Math.floor(resourcesLeft.vcpus / jobResourcesOneInstance.vcpus)
+  val mem = Math.floor(resourcesLeft.memory / jobResourcesOneInstance.memory)
+  val dev = resourcesLeft.devices.foldLeft(Integer.MAX_VALUE){
+    (max, tuple) =>
+    val (name, cnt) = tuple
+    jobResourcesOneInstance.devices.get(name) match {
+      case None => max
+      case Some(required) =>
+        Math.min(max, Math.floor(cnt / required).toInt)
     }
-    Math.min(Math.min(nCPUs.toInt, mem.toInt), dev)
   }
+  Math.min(Math.min(nCPUs.toInt, mem.toInt), dev)
+}
 
-  private def getCostHere(p: PotentialMatch) =
-  {
-    val resourcesAtTime = getTotalResourceUsage(p)
-    costToSchedule(HostWithCurrentUsage(p.h, resourcesAtTime))
-  }
+private def getCostHere(p: PotentialMatch) =
+{
+  val resourcesAtTime = getTotalResourceUsage(p)
+  costToSchedule(HostWithCurrentUsage(p.h, resourcesAtTime))
+}
 
-  /** runtime: O(times * times * hosts) */
-  private def scheduleJobThatTakesOneHost(job: BatchJob,
-                                          alreadyScheduledThisRound: List[ScheduledBatchJob]) = {
-    var outerHostID: Option[String] = None // host to schedule on
-    val firstInterval: Option[Int] = intervalsToLookAt.dropWhile{ time =>
-      hosts.foldLeft(Option.empty[(String, Long)]){ case (accum, host) =>
-        val p = PotentialMatch(host, job, time,
-                               alreadyScheduledThisRound)
-        if (0 < numHostsSchedulableHere(p)) {
-          val costHere = getCostHere(p)
-          accum match {
-            case Some((_, prevMinCost)) if prevMinCost > costHere =>
-              Some((host.id, costHere))
-            case _ => accum
-          }
-        } else accum
-      } match {
-        case Some((minCostHost, _)) =>
-          outerHostID = Some(minCostHost)
-          false
-        case _ => true
-      }
-    }.headOption
-    firstInterval map { x =>
-      ScheduledBatchJob(hostToNumInstances = Map(outerHostID.get.toString -> 1),
-                startInterval = x, job) }
-  }
-
-  private def scheduleJobThatTakesMultipleHosts(
-    job: BatchJob, alreadyScheduledThisRound: List[ScheduledBatchJob]) = {
-    import scala.collection.mutable.PriorityQueue
-    implicit val ord = Ordering.fromLessThan[HostAllocInfo]((h1, h2) => h1.cost > h2.cost) // tested in console
-    case class HostAllocInfo(cost: Long, nsched: Int, hostID: String)
-    var mcHosts: Option[PriorityQueue[HostAllocInfo]] = None
-    val firstInterval: Option[Int] = intervalsToLookAt.dropWhile { time =>
-      // should be a minheap ordered by cost, but contains tuples (cost, num_schedulable)
-      val lowestCostHosts = PriorityQueue.empty[HostAllocInfo]
-      hosts.foreach{host =>
-        val p = PotentialMatch(host, job, time, alreadyScheduledThisRound)
-        val num_schedulable: Int = numHostsSchedulableHere(p)
-        if (num_schedulable > 0) {
-          lowestCostHosts.enqueue(HostAllocInfo(cost = getCostHere(p),
-                                                nsched = num_schedulable, hostID = host.id))
+/** runtime: O(times * times * hosts) */
+private def scheduleJobThatTakesOneHost(job: BatchJob,
+                                        alreadyScheduledThisRound: List[ScheduledBatchJob]) = {
+  var outerHostID: Option[String] = None // host to schedule on
+  val firstInterval: Option[Int] = intervalsToLookAt.dropWhile{ time =>
+    hosts.foldLeft(Option.empty[(String, Long)]){ case (accum, host) =>
+      val p = PotentialMatch(host, job, time,
+                             alreadyScheduledThisRound)
+      if (0 < numHostsSchedulableHere(p)) {
+        val costHere = getCostHere(p)
+        accum match {
+          case Some((_, prevMinCost)) if prevMinCost > costHere =>
+            Some((host.id, costHere))
+          case _ => accum
         }
-      }
-      val hostsAvailable = lowestCostHosts.foldLeft(0)((accum, x) => accum + x.nsched)
-      if (hostsAvailable >= job.requiredHosts) {
-        mcHosts = Some(lowestCostHosts.clone())
+      } else accum
+    } match {
+      case Some((minCostHost, _)) =>
+        outerHostID = Some(minCostHost)
         false
-      } else true
-    }.headOption
-
-    firstInterval map { x =>
-      var hostToNumInstances: Map[String, Int] = Map()
-      mcHosts.get.iterator.takeWhile{ hai =>
-        val total = hostToNumInstances.foldLeft(0){(accum, tuple) =>
-          val (k, v) = tuple
-          accum + v}
-        Integer.compare(total, job.requiredHosts) match {
-          case -1 => false
-          case 0 => false
-          case _ =>
-            val nWanted = job.requiredHosts - total
-            val nTaken = Math.max(nWanted, hai.nsched)
-            hostToNumInstances = hostToNumInstances + (hai.hostID.toString -> nTaken)
-            true
-        }
-      }
-
-      ScheduledBatchJob(hostToNumInstances = hostToNumInstances, startInterval = x, job)
+      case _ => true
     }
-  }
+  }.headOption
+  firstInterval map { x =>
+    ScheduledBatchJob(hostToNumInstances = Map(outerHostID.get.toString -> 1),
+                      startInterval = x, job) }
+}
 
-  /**
-   produces a set of those jobs that were scheduled
-    if running this for the first time, can simply create a dummy batch schedule with an empty
-    job list
-
-    perhaps this should return a Future[List[Scheduled]]?
-   */
-  def runSchedulingAlgorithm()(implicit ec: ExecutionContext): Future[List[ScheduledBatchJob]] = Future {
-    val orderedJobsToSchedule =
-      jobsToSchedule.sortWith((l, r) =>
-        // true iff l goes before r
-        l.inserted.isBefore(r.inserted) &&
-          l.adminSetPriority > r.adminSetPriority
-      )
-    var result: List[ScheduledBatchJob] = List.empty
-    orderedJobsToSchedule.iterator.takeWhile{ job =>
-      val schedulingResult = job.requiredHosts match {
-        case 1 => scheduleJobThatTakesOneHost(job, result)
-        case _ => scheduleJobThatTakesMultipleHosts(job, result)
+private def scheduleJobThatTakesMultipleHosts(
+  job: BatchJob, alreadyScheduledThisRound: List[ScheduledBatchJob]) = {
+  import scala.collection.mutable.PriorityQueue
+  implicit val ord = Ordering.fromLessThan[HostAllocInfo]((h1, h2) => h1.cost > h2.cost) // tested in console
+  case class HostAllocInfo(cost: Long, nsched: Int, hostID: String)
+  var mcHosts: Option[PriorityQueue[HostAllocInfo]] = None
+  val firstInterval: Option[Int] = intervalsToLookAt.dropWhile { time =>
+    // should be a minheap ordered by cost, but contains tuples (cost, num_schedulable)
+    val lowestCostHosts = PriorityQueue.empty[HostAllocInfo]
+    hosts.foreach{host =>
+      val p = PotentialMatch(host, job, time, alreadyScheduledThisRound)
+      val num_schedulable: Int = numHostsSchedulableHere(p)
+      if (num_schedulable > 0) {
+        lowestCostHosts.enqueue(HostAllocInfo(cost = getCostHere(p),
+                                              nsched = num_schedulable, hostID = host.id))
       }
-      schedulingResult match {
-        case None => false
-        case Some(s) =>
-          s :: result
+    }
+    val hostsAvailable = lowestCostHosts.foldLeft(0)((accum, x) => accum + x.nsched)
+    if (hostsAvailable >= job.requiredHosts) {
+      mcHosts = Some(lowestCostHosts.clone())
+      false
+    } else true
+  }.headOption
+
+  firstInterval map { x =>
+    var hostToNumInstances: Map[String, Int] = Map()
+    mcHosts.get.iterator.takeWhile{ hai =>
+      val total = hostToNumInstances.foldLeft(0){(accum, tuple) =>
+        val (k, v) = tuple
+        accum + v}
+      Integer.compare(total, job.requiredHosts) match {
+        case -1 => false
+        case 0 => false
+        case _ =>
+          val nWanted = job.requiredHosts - total
+          val nTaken = Math.max(nWanted, hai.nsched)
+          hostToNumInstances = hostToNumInstances + (hai.hostID.toString -> nTaken)
           true
       }
     }
-    result
+
+    ScheduledBatchJob(hostToNumInstances = hostToNumInstances, startInterval = x, job)
   }
+}
+
+case class SchedulerInfo(hosts: Set[Host], brickIDToUsage: Map[String, HostResources],
+                         runningJobs: List[RunningBatchJob], intervalsToLookAt: Range,
+                         jobsToSchedule: List[BatchJob],
+                         hostResCreator: HostResourcesCreator,
+                         costToSchedule: HostWithCurrentUsage => Long)
+
+def runSchedulingAlgorithm()(implicit ec: ExecutionContext): Future[List[ScheduledBatchJob]] = Future {
+  val orderedJobsToSchedule =
+    jobsToSchedule.sortWith((l, r) =>
+      // true iff l goes before r
+      l.inserted.isBefore(r.inserted) &&
+        l.adminSetPriority > r.adminSetPriority
+    )
+  var result: List[ScheduledBatchJob] = List.empty
+  orderedJobsToSchedule.iterator.takeWhile{ job =>
+    val schedulingResult = job.requiredHosts match {
+      case 1 => scheduleJobThatTakesOneHost(job, result)
+      case _ => scheduleJobThatTakesMultipleHosts(job, result)
+    }
+    schedulingResult match {
+      case None => false
+      case Some(s) =>
+        s :: result
+        true
+    }
+  }
+  result
 }
